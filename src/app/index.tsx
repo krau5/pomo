241589f557f756debef1ec2fd1ad3b5f90440c1b
krau5 <apvbd17@gmail.com>
2022-10-30 23:09:03 +0100

    Dynamic imports for each page ⚡️

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,27 @@
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
 import { Layout } from 'components/Layout';
-import { Home } from 'pages/Home';
-import { Login } from 'pages/Login';
-import { SignUp } from 'pages/SignUp';
 import { AppProvider } from 'app/AppProvider';
 import { UIProvider } from 'app/UIProvider';
 import { Routes } from 'constants/routes';
+import { lazy } from 'react';
+
+const Home = lazy(() =>
+  import('pages/Home').then((module) => ({
+    default: module.Home,
+  }))
+);
+
+const Login = lazy(() =>
+  import('pages/Login').then((module) => ({
+    default: module.Login,
+  }))
+);
+
+const SignUp = lazy(() =>
+  import('pages/SignUp').then((module) => ({
+    default: module.SignUp,
+  }))
+);
 
 export const App = () => (
   <UIProvider>
