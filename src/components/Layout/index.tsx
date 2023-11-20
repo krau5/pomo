c0ff913bf3aa414e382a86dcd3a8c6391c1e710d
krau5 <apvbd17@gmail.com>
2023-11-20 14:42:23 +0100

    Move vite-tsconfig-paths to devDeps, useThemedFavicon hook

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,20 +1,9 @@
-import { PropsWithChildren, useEffect } from 'react';
+import { PropsWithChildren } from 'react';
 import { Box } from 'components/Box';
-import { useAppSelector } from 'store';
-import { selectCurrentInterval } from 'store/intervals';
-import { selectTheme } from 'store/theme';
-
-const link = document.getElementById('favicon') as HTMLLinkElement;
+import { useThemedFavicon } from 'hooks';
 
 export const Layout = ({ children }: PropsWithChildren) => {
-  const currentInterval = useAppSelector(selectCurrentInterval);
-  const theme = useAppSelector(selectTheme);
-
-  useEffect(() => {
-    if (link) {
-      link.href = `/images/favicon/${currentInterval}-${theme}.svg`;
-    }
-  }, [currentInterval, theme]);
+  useThemedFavicon();
 
   return (
     <Box
---
+++ src/hooks/useThemedFavicon.ts
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useAppSelector } from 'store';
+import { selectCurrentInterval } from 'store/intervals';
+import { selectTheme } from 'store/theme';
+
+const link = document.getElementById('favicon') as HTMLLinkElement;
+
+export const useThemedFavicon = () => {
+  const currentInterval = useAppSelector(selectCurrentInterval);
+  const theme = useAppSelector(selectTheme);
+
+  useEffect(() => {
+    if (link) {
+      link.href = `/images/favicon/${currentInterval}-${theme}.svg`;
+    }
+  }, [currentInterval, theme]);
+};
